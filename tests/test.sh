a5c4edf78d7d0d4e0408006b9e3e0d9a0138bf4a
Hildo Guillardi Júnior <hildogjr@users.noreply.github.com>
2017-09-03 12:06:46 -0300

    Macro to the KiCost
    This macro in bash (Linux prompt language, but also available in Windows 10 via the Microsoft update) test all the XML BOM files present in the folder.
    If finish the script, all the passed all print this in the prompt. If don't, return error.
    I am using to validate my last changes in the KiCost.

---
+++ tests/test.sh
@@ -0,0 +1,37 @@
+# Automatic test macro for KiCad.
+# Use this script in linux to generate the spreadsheet based on the XML files of this folder after changes in the KiCost python module. Use to validate and check erros.
+# Written by Hildo Guillardi Júnior
+# Use in Linux or Microsoft Windows with bash capability
+
+# MIT license
+#
+# Copyright (C) 2015 by XESS Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+echo 'This macro test all the xml files BOM in this folder'
+BOMs=$(find *.xml)
+echo $(echo "$var" | wc -l)" recognized files"
+echo "$BOMs"
+for eachBOM in $BOMs; do
+    echo "############ Testing file $eachBoM"
+    kicost -i $eachBOM --overwrite
+done
+
+echo "If you see this message all BOMs spreadsheet was created without error"
