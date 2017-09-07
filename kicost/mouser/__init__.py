398adf64b0849a3ca559afd07101c50f30e480b6
Hildo Guillardi Júnior <hildogjr@users.noreply.github.com>
2017-09-07 12:41:59 -0300

    Delete __init__.py

--- kicost/mouser/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'XESS Corporation'
-__email__ = 'info@xess.com'
-
-from .mouser import get_mouser_price_tiers, get_mouser_part_num, get_mouser_qty_avail, get_mouser_part_html_tree
