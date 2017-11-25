91a870b15fce4d26c02c284f9ff6a7628a9860d8
Hildo Guillardi Júnior <hildogjr@users.noreply.github.com>
2017-11-25 14:20:54 -0200

    Fixed mistakes interpretation
    Fixed mistakes in the CSV interpretation.
    Now allow multiple \t in the CSV, maybe the editor of the CSV BOM used to align the columns.

--- kicost/eda_tools/__init__.py
+++ kicost/eda_tools/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # MIT license
 #
-# Copyright (C) 2015 by XESS Corporation
+# Copyright (C) 2015 by XESS Corporation / Hildo G Jr
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
