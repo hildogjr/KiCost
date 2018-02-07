84c46be536bdfe3e34d98a64636ba5c8c11591a0
Hildo Guillardi Júnior <hildogjr@users.noreply.github.com>
2018-02-07 13:59:11 -0200

    Add informations to `setup.py`

--- setup.py
+++ setup.py
@@ -39,7 +39,11 @@ setup(
     long_description=readme + '\n\n' + history,
     author=kicost.__author__,
     author_email=kicost.__email__,
-    url='https://github.com/xesscorp/KiCost',
+    url='https://xesscorp.github.io/KiCost',
+    project_urls={
+        'Doc': 'https://xesscorp.github.io/KiCost',
+        'Git': 'https://github.com/xesscorp/KiCost',
+    }
     packages=setuptools.find_packages(),
     entry_points={'console_scripts':['kicost = kicost.__main__:main']},
     package_dir={'kicost':'kicost'},
