4e3c7ecc8e4ef40b4bc589cbab52ef2d34155b08
Max Maisel <max.maisel@posteo.de>
2018-06-01 15:18:13 +0200

    Started implementation of fake_browser and distributor base classes.

---
+++ kicost/distributors/distributor.py
@@ -0,0 +1,211 @@
+# MIT license
+#
+# Copyright (C) 2018 by XESS Corporation / Max Maisel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Author information.
+__author__ = 'Max Maisel'
+__webpage__ = 'https://github.com/mmmaisel/'
+
+# Libraries.
+import sys
+from bs4 import BeautifulSoup # XML file interpreter.
+import multiprocessing # To deal with the parallel scrape.
+import logging
+import time
+from random import choice
+from ..eda_tools.eda_tools import order_refs # To better print the warnings about the parts.
+
+from . import fake_browser
+
+import http.client # For web scraping exceptions.
+try:
+    # This is for Python 3.
+    from urllib.parse import urlencode, quote_plus as urlquote, urlsplit, urlunsplit
+    from urllib.request import urlopen, Request
+    import urllib.error
+    WEB_SCRAPE_EXCEPTIONS = (urllib.error.URLError, http.client.HTTPException)
+except ImportError:
+    # This is for Python 2.
+    from urlparse import urlsplit, urlunsplit
+    from urllib import urlencode, quote_plus as urlquote
+    from urllib2 import urlopen, Request
+    import urllib2
+    WEB_SCRAPE_EXCEPTIONS = (urllib2.URLError, http.client.HTTPException)
+
+from ..globals import logger, DEBUG_OVERVIEW, DEBUG_DETAILED, DEBUG_OBSESSIVE # Debug configurations.
+from ..globals import SEPRTR
+from ..globals import PartHtmlError
+from . import distributor_dict
+
+import os, re
+
+class distributor:
+    def __init__(self, scrape_retries, log_level, throttle_delay):
+        self.name = None
+        self.page_accessed = False
+        self.scrape_retries = scrape_retries
+        self.logger = logger
+        self.log_level = log_level
+        self.throttle_delay = throttle_delay
+        self.throttle_timeout = time.time()
+        self.domain = None
+        self.browser = fake_browser.fake_browser(self.logger, self.scrape_retries)
+
+    # Abstract methods, implemented in distributor specific modules
+    def dist_get_part_html_tree(self, pn, extra_search_terms, url, descend):
+        raise NotImplementedError()
+
+    def dist_get_part_num(self, html_tree):
+        raise NotImplementedError()
+
+    def dist_get_qty_avail(self, html_tree):
+        raise NotImplementedError()
+
+    def dist_get_price_tiers(self, html_tree):
+        raise NotImplementedError()
+
+    def dist_get_extra_info(self, html_tree):
+        raise NotImplementedError()
+
+    def dist_define_locale_currency(self, locale, currency):
+        raise NotImplementedError()
+
+    def define_locale_currency(self, locale_currency='USD'):
+        '''@brief Configure the distributor for some locale/country and
+        currency second ISO3166 and ISO4217
+        
+        @param `str` Alpha 2 country or alpha 3 currency or even one slash other.'''
+        try:
+            if distributor_dict[self.name]['scrape'] == 'web':
+                # Not make sense to configurate a local distributor (yet).
+                locale_currency = re.findall('\w{2,}', locale_currency)
+                locale = None
+                currency = None
+                for alpha in locale_currency:
+                    if len(alpha)==2:
+                        locale = alpha
+                    elif len(alpha)==3:
+                        currency = alpha
+                self.dist_define_locale_currency(locale, currency)
+        except NotImplementedError:
+            logger.warning('No currency/country configuration for {}.'.format(self.name))
+            pass
+
+    def scrape_part(self, id, part, local_part_html):
+        '''@brief Scrape the data for a part from each distributor website or local HTML.
+        
+        Use distributors submodules to scrape each distributor part page and get
+        informations such as price, quantity avaliable and others;
+        
+        @param `int` Count of the main loop.
+        @param `str` String with the part number / distributor stock.
+        @param `str` Local part HTML
+        @return id, distributor_name, url, `str` distributor stock part number,
+            `dict` price tiers, `int` qty avail, `dict` extrainfo dist
+        '''
+
+        if multiprocessing.current_process().name == "MainProcess":
+            self.logger = logging.getLogger('kicost')
+        else:
+            self.logger = multiprocessing.get_logger()
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(self.log_level)
+            self.logger.addHandler(handler)
+            self.logger.setLevel(self.log_level)
+            self.browser.logger = self.logger
+
+        url = {}
+        part_num = {}
+        qty_avail = {}
+        price_tiers = {}
+        info_dist = {}
+
+        if distributor_dict[self.name]['scrape']=='web':
+            if self.page_accessed == True:
+                # Check the throttling timeout for the chosen distributor to see if
+                # another access to its website is allowed.
+                if self.throttle_timeout > time.time():
+                    time.sleep(self.throttle_timeout - time.time())
+
+            # Update the timeout for this distributor website and release the sync. lock.
+            self.throttle_timeout = time.time() + self.throttle_delay
+            # Founded manufacturer / distributor code valid (not empty).
+        else:
+            self.logger.log(DEBUG_OBSESSIVE,'No delay for %s, type=%s' \
+                % (self.name, distributor_dict[self.name]['scrape']))
+
+        # Get the HTML tree for the part.
+        html_tree, url = self.get_part_html_tree(part, local_part_html=local_part_html)
+
+        # Call the functions that extract the data from the HTML tree.
+        part_num = self.dist_get_part_num(html_tree)
+        qty_avail = self.dist_get_qty_avail(html_tree)
+        price_tiers = self.dist_get_price_tiers(html_tree)
+        
+        try:
+            # Get extra characeristics of the part in the web page.
+            # This will be use to comment in the 'cat#' column of the
+            # spreadsheet and some validations (in the future implementaions)
+            info_dist = self.dist_get_extra_info(html_tree)
+        except:
+            info_dist = {}
+            pass
+
+        # Return the part data.
+        return id, self.name, url, part_num, price_tiers, qty_avail, info_dist
+
+    def get_part_html_tree(self, part, local_part_html):
+        '''@brief Get the HTML tree for a part.
+        
+        Get the HTML tree for a part from the given distributor website or local HTML.
+        @param `str` part Part manufactor code or distributor stock code.
+        @param `str` local_part_html
+        @return `str` with the HTML webpage.'''
+
+        self.logger.log(DEBUG_OBSESSIVE, 'Looking in %s by %s:', self.name, order_refs(part.refs, True))
+
+        for extra_search_terms in set([part.fields.get('manf', ''), '']):
+            try:
+                # Search for part information using one of the following:
+                #    1) the distributor's catalog number.
+                #    2) the manufacturer's part number.
+                for key in (self.name+'#', self.name+SEPRTR+'cat#', 'manf#'):
+                    if key in part.fields:
+                        if part.fields[key]:
+                            self.page_accessed = True
+                            return self.dist_get_part_html_tree \
+                                (part.fields[key], extra_search_terms, local_part_html=local_part_html)
+                # No distributor or manufacturer number, so give up.
+                else:
+                    self.page_accessed = False
+                    self.logger.warning("No '%s#' or 'manf#' field: cannot lookup part %s at %s.", \
+                        self.name, part.refs, self.name)
+                    return BeautifulSoup('<html></html>', 'lxml'), ''
+                    #raise PartHtmlError
+            except PartHtmlError:
+                pass
+            except AttributeError:
+                break
+        self.logger.warning("Part %s not found at %s.", order_refs(part.refs, False), self.name)
+        # If no HTML page was found, then return a tree for an empty page.
+        return BeautifulSoup('<html></html>', 'lxml'), ''
+
+
---
+++ kicost/distributors/fake_browser.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+# MIT license
+#
+# Copyright (C) 2018 by XESS Corporation / Hildo Guillardi Junior / Max Maisel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+__author__ = 'XESS Corporation'
+__email__ = 'info@xess.com'
+
+from random import choice
+
+import http.client # For web scraping exceptions.
+import http.cookiejar
+
+from ..globals import DEBUG_OVERVIEW, DEBUG_DETAILED, DEBUG_OBSESSIVE
+
+try:
+    # This is for Python 3
+    from urllib.parse import urlencode, quote_plus as urlquote, urlsplit, urlunsplit
+    from urllib.request import urlopen, Request
+    import urllib.error
+    WEB_SCRAPE_EXCEPTIONS = (urllib.error.URLError, http.client.HTTPException)
+except ImportError:
+    # This is for Python 2
+    from urlparse import urlsplit, urlunsplit
+    from urllib import urlencode, quote_plus as urlquote
+    from urllib2 import urlopen, Request
+    import urllib2
+    WEB_SCRAPE_EXCEPTIONS = (urllib2.URLError, http.client.HTTPException)
+
+
+def get_user_agent():
+    ''' The default user_agent_list comprises chrome, IE, firefox, Mozilla, opera, netscape.
+      You can find more user agent strings at https://techblog.willshouse.com/2012/01/03/most-common-user-agents/.
+      Used for the function `fake_browser(url, retries)`
+    '''
+    user_agent_list = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36 OPR/49.0.2725.47",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.17",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; rv:57.0) Gecko/20100101 Firefox/57.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36 OPR/49.0.2725.39",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+        "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/62.0.3202.75 Chrome/62.0.3202.75 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/62.0.3202.94 Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;  Trident/5.0)",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (iPad; CPU OS 11_1_2 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B202 Safari/604.1",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0;  Trident/5.0)",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/62.0.3202.89 Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    ]
+    return choice(user_agent_list)
+
+# Open the URL, read the HTML from it, and parse it into a tree structure.
+class fake_browser:
+    def __init__(self, logger, scrape_retries):
+        '''@brief fake_browser
+           @param logger
+           @param scrape_retries `int` Quantity of retries in case of fail.
+        '''
+        self.cookiejar = http.cookiejar.CookieJar()
+        self.userAgent = get_user_agent()
+        self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookiejar))
+        self.scrape_retries = scrape_retries
+        self.logger = logger
+
+    def show_cookies(self, name):
+        for x in self.cookiejar:
+            # TODO: use logger
+            self.logger.log(DEBUG_OBSESSIVE,"%s Cookie %s" % (name, x.name))
+            print("%s Cookie %s" % (name, x.name))
+
+    def add_cookie(self, domain, name, value):
+        self.cookiejar.set_cookie(http.cookiejar.Cookie(
+            version=0, 
+            name=name, 
+            value=value,
+            port=None, 
+            port_specified=False,
+            domain=domain, 
+            domain_specified=True, 
+            domain_initial_dot=False,
+            path="/", 
+            path_specified=False,
+            secure=False,
+            expires=None,
+            discard=False,
+            comment=None,
+            comment_url=None,
+            rest=None))
+
+    def scrape_URL(self, url, add_header=None):
+        for _ in range(self.scrape_retries):
+            try:
+                req = Request(url)
+                if add_header:
+                    req.add_header(add_header)
+                req.add_header('User-agent', self.userAgent)
+                req.add_header('Accept', 'text/html')
+                req.add_header('Accept-Language', 'en-US')
+                req.add_header('Accept-Encoding', 'identity')
+                response = self.opener.open(req, timeout=10)
+                html = response.read()
+                break
+            #except WEB_SCRAPE_EXCEPTIONS:
+            except Exception as ex:
+                # TODO: remove print
+                print('Exception of type "%s" while web-scraping %s' \
+                    % (type(ex).__name__, format(url)))
+                self.logger.log(DEBUG_DETAILED,'Exception of type "%s" while web-scraping %s' \
+                    % (type(ex).__name__, format(url)))
+                pass
+        else:
+            # TODO: remove print
+            print('No page')
+            raise ValueError('No page')
+        return html
+
