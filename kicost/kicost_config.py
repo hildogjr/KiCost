1dd228c390a70127cae3a7bbe71d42a842166244
Hildo Guillardi Júnior <hildogjr@gmail.com>
2019-09-19 13:24:37 -0300

    Win functions file

--- kicost/kicost_config.py
+++ kicost/kicost_config.py
@@ -40,7 +40,8 @@ except:
 __all__ = ['kicost_setup', 'kicost_unsetup']
 
 WINDOWS_STARTS_WITH = 'win32'
-
+if sys.platform.startswith(WINDOWS_STARTS_WITH):
+    from .windows import *
 
 
 ###############################################################################
@@ -227,52 +228,6 @@ def add_bom_plugin_entry(kicad_config_path, name, cmd, nickname=None, re_flags=r
 ###############################################################################
 ## Auxiliary functions.
 ###############################################################################
-
-if sys.platform.startswith(WINDOWS_STARTS_WITH):
-    # Create the functions to deal with Windows registry, from http://stackoverflow.com/a/35286642
-    import winreg
-
-    def get_reg(path, name, key=winreg.HKEY_CURRENT_USER):
-        # Read variable from Windows Registry.
-        try:
-            reg = winreg.ConnectRegistry(None, key)
-            registry_key = winreg.OpenKey(reg, path, 0, winreg.KEY_READ)
-            value, regtype = winreg.QueryValueEx(registry_key, name)
-            winreg.CloseKey(reg)
-            return value
-        except WindowsError:
-            return None
-
-    def set_reg(path, name, value, key=winreg.HKEY_CURRENT_USER, key_type=winreg.REG_SZ):
-        # Write in the Windows Registry.
-        try:
-            reg = winreg.ConnectRegistry(None, key)
-            winreg.CreateKey(reg, path)
-            registry_key = winreg.OpenKey(reg, path, 0, winreg.KEY_WRITE)
-            winreg.SetValueEx(registry_key, name, 0, key_type, value)
-            winreg.CloseKey(reg)
-            # Uptade the Windows behaviour.
-            #SendMessage(win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 0, 'Environment')
-            return True
-        except PermissionError:
-            print('You shoud run this command as system administrator: run the terminal as admnistrator and type the command again.')
-        except WindowsError:
-            return False
-    
-    def del_reg(name, key=winreg.HKEY_CURRENT_USER):
-        # Delete a registry key on Windows.
-        try:
-            reg = winreg.ConnectRegistry(None, key)
-            #registry_key = winreg.OpenKey(reg, name_base, 0, winreg.KEY_ALL_ACCESS)
-            winreg.DeleteKey(reg, name)
-            winreg.CloseKey(reg)
-            # Uptade the Windows behaviour.
-            #SendMessage(win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 0, 'Environment')
-            return True
-        except PermissionError:
-            print('You shoud run this command as system administrator: run the terminal as admnistrator and type the command again.')
-        except WindowsError:
-            return False
 
 
 def install_kicad_plugin(path):
