6d3054820540981a6db9958f8fba862f1a344eb4
U-YNA007\mdeweerd <mdw_git@ynamics.com>
2020-05-27 17:23:58 +0200

    Testing with noprice
    Also removed small test from travis as it is included in the full test.

--- tests/test_single.sh
+++ tests/test_single.sh
@@ -27,6 +27,25 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+BOMs=()
+OPTS=()
+# Extract options
+parsed_options=$(
+  getopt -o hv -l noprice -- "$@"
+) || exit
+eval "set -- $parsed_options"
+while [ "$#" -gt 0 ]; do
+  case $1 in
+    (--noprice) OPTS+=("$1") ; shift;;
+    (-[hv]) shift;;  # Options without parameter
+    #(-t) shift 2;;   # Option with parameter
+    (--) shift; break;;
+    (*) BOMs+=("$1"); echo FILES "${FILES[@]}"; shift; break;; #exit 1 # should never be reached.
+  esac
+done
+BOMs+=("$*")
+#echo OPTIONS "${OPTS[@]}"
+#echo BOMs "${FILES[@]}"
 
 echo 'This macro tests selected xml or csv BOM files in this folder'
 cd $(dirname $0)
@@ -34,24 +53,26 @@ cd $(dirname $0)
 RESULT_PATH=result_test/
 EXPECT_PATH=expected_test/
 LOG_PATH=log_test/
+
 # Create the target directory for test files to compare if needed
 mkdir -p ${RESULT_PATH}
 mkdir -p ${LOG_PATH}
 # Remove previous results if any
-rm ${RESULT_PATH}*
-
 
 RESULT=0
 
-for eachBOM in "$@" ; do
+for eachBOM in "${BOMs[@]}" ; do
     echo "##### Testing file: $eachBOM"
+    rm "${RESULT_PATH}${eachBOM%.*}.csv" >& /deV/null
+    rm "${LOG_PATH}${eachBOM%.*}.log" >& /deV/null
+
     if [[ ${eachBOM#*.} == "csv" ]] ; then
-       kicost -wi "$eachBOM" --debug=10 --eda csv >& "${LOG_PATH}$eachBOM".log
+       kicost "${OPTS[@]}" -wi "$eachBOM" --debug=10 --eda csv >& "${LOG_PATH}$eachBOM".log
     else
-       kicost -wi "$eachBOM" --debug=10 >& "${LOG_PATH}$eachBOM".log
+       kicost "${OPTS[@]}" -wi "$eachBOM" --debug=10 >& "${LOG_PATH}$eachBOM".log
     fi
     # Convert Excel to CSV file to make simple verification
-    xlsx2csv --skipemptycolumns "${eachBOM%.*}.xlsx" | egrep -i -v '(USD\(| date|kicost)' > "${RESULT_PATH}${eachBOM%.*}.csv"
+    xlsx2csv --skipemptycolumns "${eachBOM%.*}.xlsx" | egrep -i -v '(USD\(| date|kicost|Total purchase)' > "${RESULT_PATH}${eachBOM%.*}.csv"
     # RESULT counts the number of errors (non 0 exit is error)
     diff "${EXPECT_PATH}${eachBOM%.*}.csv" "${RESULT_PATH}${eachBOM%.*}.csv"
     RESULT=$(($RESULT + $?))
@@ -59,7 +80,7 @@ for eachBOM in "$@" ; do
 done
 
 if [[ ${RESULT} == 0 ]] ; then
-  echo "If you see this message all BOMs spreadsheet was created without error"
+  echo "If you see this message all BOMs spreadsheet were created without error"
 else
   echo "If you see this message there were some unexpected results"
 fi
