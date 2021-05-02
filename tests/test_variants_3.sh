ff9979168818e995583b8816f926cfce9cb72f7d
MDW <--unset>
2021-05-02 17:50:29 +0200

    Removed test_variants_3.sh, integrated in test_kicost.py

--- tests/test_variants_3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xv
-#kicost --no_price --variant variant1 -wi variants_3.xml --debug=10
-
-$(dirname $0)/test_single.sh --no_price variants_3.xml
-$(dirname $0)/test_single.sh --no_price --variant variant1 variants_3.xml
-#cat log_test/variants_3.xml.log
-
