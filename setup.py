c3fa787470494261a5a4b3d01f68a1e18353b1bf
Salvador E. Tropea <salvador@inti.gob.ar>
2021-10-18 14:35:51 -0300

    Added Python 2.7 and 3.5 to setup.py.
    - The code is passing trivial tests (using cached data) with both.

--- setup.py
+++ setup.py
@@ -154,6 +154,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
